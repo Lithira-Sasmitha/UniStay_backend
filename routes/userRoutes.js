473ebfb3e7c75711ee270a0ab02c1a6c00756134
LITHIRA-SASMITHA <lithirasasmitha@gmail.com>
2026-02-24 00:03:28 +0530

    connect db

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getAllUsers, createUser } = require('../controllers/userController');
 
-// Map endpoints to controller functions
 router.route('/').get(getAllUsers).post(createUser);
 
 module.exports = router;
